c74d355fbcf0d32f0edfd919c2f10bcb12d9fc95
David-Pikal <david.pikal0@gmail.com>
2022-04-02 13:26:30 +0200

    Two functional microservices.

--- album/src/main/java/cz/uhk/album/HomeController.java
+++ album/src/main/java/cz/uhk/album/HomeController.java
@@ -1,12 +1,14 @@
 package cz.uhk.album;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/albums")
 public class HomeController {
     
-    @GetMapping
+    @GetMapping("/home")
     public String home() {
         return "Hello from 2nd microservice!";
     }
