c9d4be25e62fa46dbde1416022dd1a055f710b82
David-Pikal <david.pikal0@gmail.com>
2022-04-09 10:14:50 +0200

    Added flux endpoint in Album microservice.

--- album/src/main/java/cz/uhk/album/HomeController.java
+++ album/src/main/java/cz/uhk/album/HomeController.java
@@ -1,16 +1,27 @@
 package cz.uhk.album;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.CoreSubscriber;
+import reactor.core.publisher.Mono;
 
+@Slf4j
 @RestController
 @RequestMapping("/albums")
 public class HomeController {
     
     @GetMapping("/home")
     public String home() {
+        log.info("Hit home endpoint in Album microservice!");
         return "Hello from 2nd microservice!";
+    }
+
+    @GetMapping("/bullshit")
+    public Mono<String> bullshit() {
+        log.info("Bullshit endpoint reached!");
+        return Mono.just("bullshit 2");
     }
     
 }
--- api-gateway/src/main/java/cz/uhk/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/cz/uhk/apigateway/ApiGatewayApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * Routes configured in application.yaml
+ */
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ApiGatewayApplication {
