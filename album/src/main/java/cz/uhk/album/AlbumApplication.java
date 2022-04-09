dbf3ab26129e86b2b794a40d3da40fa66309fac5
David-Pikal <david.pikal0@gmail.com>
2022-04-09 18:55:28 +0200

    Zuul proxy - dead end.

--- album/src/main/java/cz/uhk/album/AlbumApplication.java
+++ album/src/main/java/cz/uhk/album/AlbumApplication.java
@@ -3,6 +3,15 @@ package cz.uhk.album;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoderFactory;
+import org.springframework.security.oauth2.jwt.JwtException;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -10,6 +19,18 @@ public class AlbumApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlbumApplication.class, args);
+	}
+
+	@Configuration
+	@EnableWebSecurity
+	static class OktaOAuth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+					.authorizeRequests().anyRequest().authenticated()
+					.and()
+					.oauth2ResourceServer().jwt();
+		}
 	}
 
 }
--- api-gateway/src/main/java/cz/uhk/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/cz/uhk/apigateway/ApiGatewayApplication.java
@@ -1,18 +1,76 @@
 package cz.uhk.apigateway;
 
+import feign.RequestInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Collections;
 
 /**
  * Routes configured in application.yaml
  */
+@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	/**
+	 * TODO - separate class
+	 * Gateway behave like Resource Server
+	 */
+	@Configuration
+	static class OktaOAuth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+					.authorizeRequests().anyRequest().authenticated()
+					.and()
+					.oauth2Login()
+					.and()
+					.oauth2ResourceServer().jwt();
+		}
+	}
+
+
+	/**
+	 * TODO
+	 * If you do use a CORS filter like this one, I recommend you change the origins, methods, and headers to be more specific, increasing security.
+	 * @return
+	 */
+	@Bean
+	public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(Collections.singletonList("*"));
+		config.setAllowedMethods(Collections.singletonList("*"));
+		config.setAllowedHeaders(Collections.singletonList("*"));
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
+	}
+
+	@Bean
+	public RequestInterceptor getUserFeignClientInterceptor(OAuth2AuthorizedClientService clientService) {
+		return new UserFeignClientInterceptor(clientService);
 	}
 
 }
